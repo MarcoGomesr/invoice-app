f1c77425a9ce7526d5b134581508cf4d2139b71e
Marco <marcogomesr@gmail.com>
2025-03-15 09:08:52 +0100

    add pending status button

---
+++ src/components/ui/SubmitButtton.tsx
@@ -0,0 +1,21 @@
+"use client"
+import { useFormStatus } from "react-dom"
+import { Button } from "./button"
+import { Loader2 } from "lucide-react"
+
+export function SubmitButtton() {
+  const { pending } = useFormStatus()
+  return (
+    <>
+      {pending ? (
+        <Button disabled className="w-full">
+          <Loader2 className="mr-2 size-4 animate-spin" /> Please wait...
+        </Button>
+      ) : (
+        <Button type="submit" className="w-full">
+          Submit
+        </Button>
+      )}
+    </>
+  )
+}
