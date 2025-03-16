724c2f37489ce7d0be9bd257563f3f978dab6e42
Marco <marcogomesr@gmail.com>
2025-03-16 15:57:30 +0100

    create  actions

---
+++ src/lib/actions.ts
@@ -0,0 +1,66 @@
+"use server"
+import { parseWithZod } from "@conform-to/zod"
+import { requireUser } from "./hooks"
+import { invoiceSchema, onboardingShema } from "./zodSchemas"
+import { prisma } from "./db"
+import { redirect } from "next/navigation"
+
+export async function onboardUser(prevState: any, formData: FormData) {
+  const session = await requireUser()
+
+  const submission = parseWithZod(formData, {
+    schema: onboardingShema
+  })
+
+  if (submission.status !== "success") {
+    return submission.reply()
+  }
+
+  const data = await prisma.user.update({
+    where: {
+      id: session.user?.id
+    },
+    data: {
+      firstName: submission.value.firstName,
+      lastName: submission.value.lastName,
+      address: submission.value.address
+    }
+  })
+
+  return redirect("/dashboard")
+}
+
+export async function createInvoice(prevState: any, FormData: FormData) {
+  const session = await requireUser()
+
+  const submission = parseWithZod(FormData, {
+    schema: invoiceSchema
+  })
+
+  if (submission.status !== "success") {
+    return submission.reply()
+  }
+
+  const data = await prisma.invoice.create({
+    data: {
+      invoiceName: submission.value.invoiceName,
+      total: submission.value.total,
+      status: submission.value.status,
+      date: submission.value.date,
+      dueDate: submission.value.dueDate,
+      fromName: submission.value.fromName,
+      fromEmail: submission.value.fromEmail,
+      fromAddress: submission.value.fromAddress,
+      clientName: submission.value.clientName,
+      clientEmail: submission.value.clientEmail,
+      clientAddress: submission.value.clientAddress,
+      currency: submission.value.currency,
+      invoiceNumber: submission.value.invoiceNumber,
+      note: submission.value.note,
+
+      invoiceItemDescription: submission.value.invoiceItemDescription,
+      invoiceItemQuantity: submission.value.invoiceItemQuantity,
+      invoiceItemRate: submission.value.invoiceItemRate
+    }
+  })
+}
