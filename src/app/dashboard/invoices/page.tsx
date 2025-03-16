bd592e4ae8d742e65eaff909f09db51e326b9808
Marco <marcogomesr@gmail.com>
2025-03-16 18:52:38 +0100

    add invoice list component

---
+++ src/app/dashboard/invoices/page.tsx
@@ -0,0 +1,33 @@
+import InvoiceList from "./InvoiceList"
+import { buttonVariants } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from "@/components/ui/card"
+
+import { PlusIcon } from "lucide-react"
+import Link from "next/link"
+
+export default function InvoicesPage() {
+  return (
+    <Card>
+      <CardHeader>
+        <div className="flex items-center justify-between">
+          <div>
+            <CardTitle className="text-2xl font-bold">Invoices</CardTitle>
+            <CardDescription>Manage your invoices</CardDescription>
+          </div>
+          <Link href="/dashboard/invoices/create" className={buttonVariants()}>
+            <PlusIcon /> Create invoice
+          </Link>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <InvoiceList />
+      </CardContent>
+    </Card>
+  )
+}
