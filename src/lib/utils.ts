fd39dfe8bdd3030f02a275c247ef7619a8eab57f
Marco <marcogomesr@gmail.com>
2025-03-16 18:50:35 +0100

    add format currency util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+type formatCurrencyProps = {
+  amount: number
+  currency: "USD" | "EUR"
+}
+
+export function formatCurrency({ amount, currency }: formatCurrencyProps) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency
+  }).format(amount)
+}
