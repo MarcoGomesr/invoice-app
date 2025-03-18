367bd0b4f1337d05c692f8f51e8491364784d7e7
Marco <marcogomesr@gmail.com>
2025-03-18 21:21:20 +0100

    fix verify ui

--- src/app/(pages)/verify/page.tsx
+++ src/app/(pages)/verify/page.tsx
@@ -12,9 +12,9 @@ import Link from "next/link"
 
 export default function Verify() {
   return (
-    <div className="flex min-h-screen w-full items-center justify-center">
-      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
-        <div className="absolute top-0 right-0 bottom-0 left-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]">
+    <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+      <div className="absolute top-0 right-0 bottom-0 left-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]">
+        <div className="flex min-h-screen w-full items-center justify-center">
           <Card className="w-[385px] px-5">
             <CardHeader className="text-center">
               <div className="mx-auto mb-4 flex size-20 items-center justify-center rounded-full bg-blue-100">
