c6574b84befc6057f87b71ca579c37e6edb10d89
Marco <marcogomesr@gmail.com>
2025-03-18 19:00:20 +0100

    add assets for landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Hero from "@/components/Hero"
+import { Navbar } from "@/components/Navbar"
+
 export default function HomePage() {
-  return d
+  return (
+    <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <Navbar />
+      <Hero />
+    </main>
+  )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link"
+import { HandCoins } from "lucide-react"
+import { RainbowButton } from "./magicui/rainbow-button"
+
+export function Navbar() {
+  return (
+    <div className="flex items-center justify-between py-5">
+      <Link href="/" className="flex items-center gap-2">
+        <HandCoins className="size-7 rounded-md bg-blue-500 p-1 text-white" />{" "}
+        <p className="text-xl font-bold">
+          Invoice <span className="text-blue-500">Marco</span>
+        </p>
+      </Link>
+      <Link href="/login">
+        <RainbowButton>Get Started</RainbowButton>
+      </Link>
+    </div>
+  )
+}
